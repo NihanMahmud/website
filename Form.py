785293b42f6fe2146bc511730c4a66b99d5f592c
NihanMahmud <mahmudnihan37@gmail.com>
2020-03-04 17:48:04 +0600

    First commit

--- Form.py
+++ Form.py
@@ -13,9 +13,11 @@ def main():
 def main2():
     username=request.form['Name'] 
     email=request.form['Email']
+    username=username.replace(' ','')
+    if username in info.keys() or email in info.values():
+        return render_template('taken.html')
     info[username]=email
     user_name=username
-    user_email=email
     return render_template('NextPage.html',name=username)
 
 @app.route('/login')
@@ -26,16 +28,24 @@ def main3():
 def main4():
     username=request.form['username']
     email=request.form['email']
+    username=username.replace(' ','')
     if username in info.keys() and email==info.get(username):
-        return render_template('profile.html', username=username)
+        return render_template('profile.html', username=username,email=email)
     else:
         return render_template('Wrong.html')         
 @app.route('/profile/receivedata',methods=['POST'])
 def main5():
     text=request.form['text']
-    user_name=request.form['username']
-    # with open(f'Info/info_.txt','a') as file:
-    #     file.write(text)
-    print(user_name)
-    return 'Data received successfully'
+    username=request.form['name']
+    email=request.form['email']
+    with open(f'Info/{username}.txt','a') as file:
+        file.write(text)
+    return render_template('receivedata.html',name=username,email=email)
+@app.route('/profile/readdata',methods=['POST'])
+def main6():
+    username=request.form['username']
+    email=request.form['email']
+    with open(f'Info/{username}.txt','r') as file:
+        text=file.read()
+    return f'Your Data :\n{text}'+render_template('readdata.html',username=username,email=email)
 app.run(debug=True)
